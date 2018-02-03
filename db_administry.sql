77b8c9730f83ea26437c0cb8933f629a8f69c4b1
imsakhaqiqy <imsakhaqiqy24@gmail.com>
2018-02-04 04:25:40 +0700

    this db

---
+++ db_administry.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Trial v11.31 (32 bit)
+MySQL - 10.1.30-MariaDB : Database - db_administry
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_administry` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_administry`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `username` varchar(10) NOT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `bagian_id` int(2) DEFAULT NULL,
+  `password` varchar(8) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `deleted` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`,`username`,`password`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`email`,`bagian_id`,`password`,`created_at`,`updated_at`,`deleted`) values (1,'imsak','imsak@email.com',1,'imsak','2018-02-04 00:00:00','2018-02-04 00:00:00',0),(2,'admin','admin@email.com',2,'admin','2018-02-04 00:00:00','2018-02-04 00:00:00',0),(3,'echo','echo@email.com',3,'echo','2018-02-04 00:00:00','2018-02-04 00:00:00',0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
