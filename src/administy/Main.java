7f4268da53d0902ed345d1557e5b0ddfadfa9a9f
trioirwanto <trio.irwanto88@gmail.com>
2018-02-17 02:55:35 +0700

    tes

--- src/administy/Main.java
+++ src/administy/Main.java
@@ -1065,6 +1065,7 @@ public class Main extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        //kmen
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
